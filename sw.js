5b4f0efb68918d4c5472498f1e4414324ba1ad98
wsc291110 <wsc291110@gmail.com>
2026-05-13 14:47:47 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-const CACHE_NAME = "bb-recipe-cache-v1";
-
-const urlsToCache = [
-  "/",
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
